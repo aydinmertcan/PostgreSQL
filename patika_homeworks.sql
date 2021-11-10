56d3cb360e70e5ae65cc5e3073e359e1be90186c
Mert Can Aydın <88203746+aydinmertcan@users.noreply.github.com>
2021-11-10 19:52:49 +0300

    Homework 10 is added.

--- patika_homeworks.sql
+++ patika_homeworks.sql
@@ -151,3 +151,16 @@ select ci.city, co.country from city ci inner join country co on ci.country_id =
 select c.first_name, c.last_name from customer as c inner join payment as p on c.customer_id = p.customer_id;
 -- 3) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 select r.rental_id, c.first_name, c.last_name from customer c inner join rental as r on c.customer_id = r.customer_id;
+
+--------------------------------------- Homework 10 --------------------------------------- 
+-- 1) city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini
+-- birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+select ci.city, co.country from city ci left join country co on ci.country_id = co.country_id;
+
+-- 2) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini
+-- birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+select p.payment_id,c.first_name,c.last_name from customer c right join payment p on c.customer_id = p.customer_id;
+
+-- 3) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini
+--birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+select r.rental_id,c.first_name,c.last_name from rental r full join customer c on r.customer_id = c.customer_id;
